b95763b985d88cf2017c44fe5bd7f260d732a97c
Dirk Mahler <dirk.mahler@buschmais.com>
2019-11-15 18:27:38 +0100

    fixed javadoc

--- src/main/java/org/jqassistant/contrib/plugin/asciidocreport/include/RuleFilter.java
+++ src/main/java/org/jqassistant/contrib/plugin/asciidocreport/include/RuleFilter.java
@@ -9,7 +9,6 @@ import java.util.Set;
 import java.util.function.Consumer;
 
 import org.apache.commons.io.FilenameUtils;
-import org.jqassistant.contrib.plugin.asciidocreport.RuleResult;
 
 /**
  * A filter for rules.
@@ -22,7 +21,7 @@ import org.jqassistant.contrib.plugin.asciidocreport.RuleResult;
 public class RuleFilter<T> {
 
     /**
-     * Match {@link RuleResult}s by the given filter.
+     * Match rule ids by the given filter.
      *
      * Accepts a comma separated list of filter patterns, each one may contain
      * wildcards "*" or "?". Any pattern starting with "!" will be interpreted as
@@ -32,7 +31,7 @@ public class RuleFilter<T> {
      *            The rules ids to match.
      * @param filter
      *            The filter.
-     * @return The matching {@link RuleResult}s.
+     * @return The matching rule ids.
      */
     public Set<String> match(Iterable<String> rules, String filter) {
         Set<String> matches = new HashSet<>();
